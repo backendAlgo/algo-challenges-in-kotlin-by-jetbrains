fab0eaa5883ae374b33a476294b2590dbd132bc3
mirshod <mmirjonov@mwiah.co.uk>
2023-11-20 18:39:14 +0500

    brute force last question added

--- Brute Force/Generate Partitions/src/Partition.kt
+++ Brute Force/Generate Partitions/src/Partition.kt
@@ -1,2 +1,11 @@
+data class Partition(val terms: List<Int>) {
+    val counterMap: Map<Int, Int> = terms.groupingBy { it }.eachCount()
+    override fun equals(other: Any?): Boolean {
+        other as Partition
+        return this.counterMap == other.counterMap
+    }
 
-data class Partition(val terms: List<Int>)
+    override fun hashCode(): Int {
+        return counterMap.hashCode()
+    }
+}
--- Brute Force/Generate Partitions/src/Task.kt
+++ Brute Force/Generate Partitions/src/Task.kt
@@ -1,17 +1,39 @@
+class PartitionGenerator(private val n: Int) {
+    private val partitions = mutableListOf<Partition>()
+    private val currentPartition = ArrayDeque<Int>()
+
+    fun generate(): List<Partition> {
+        doGenerate(n, 1)
+        return partitions.toList()
+    }
+
+    private fun doGenerate(n: Int, lastAddend: Int) {
+        if (n == 0) {
+            partitions.add(Partition(currentPartition.toList()))
+            return
+        }
+        if (n < lastAddend) {
+            return
+        }
 
 
-// Implement additional functions and classes if required
+        for (currentAddend in lastAddend..n) {
+            currentPartition.addLast(currentAddend)
+            doGenerate(n - currentAddend, currentAddend)
+            currentPartition.removeLast()
+        }
+    }
+}
+
 
 fun generatePartitions(n: Int): List<Partition> {
-    return if (n == 4) {
-        listOf(
-            Partition(listOf(1, 1, 1, 1)),
-            Partition(listOf(1, 1, 2)),
-            Partition(listOf(1, 3)),
-            Partition(listOf(2, 2)),
-            Partition(listOf(4)),
-        )
-    } else {
-        TODO("unimplemented")
-    }
+    val generator = PartitionGenerator(n)
+    return generator.generate()
+}
+
+fun main() {
+    println(generatePartitions(4))
+//    val map1 = listOf(1, 1, 2, 1).groupingBy { it }.eachCount()
+//    val map2 = listOf(1, 1, 1, 2).groupingBy { it }.eachCount()
+//    println(map1 == map2)
 }
