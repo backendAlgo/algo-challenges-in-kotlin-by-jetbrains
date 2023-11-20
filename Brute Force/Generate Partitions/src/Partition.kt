b8cfa28b930fdcfe770e51c33eed8f151e81dafe
mirshod <mmirjonov@mwiah.co.uk>
2023-11-20 18:56:08 +0500

    brute force done

--- Brute Force/Generate Partitions/src/Partition.kt
+++ Brute Force/Generate Partitions/src/Partition.kt
@@ -1,11 +1,1 @@
-data class Partition(val terms: List<Int>) {
-    val counterMap: Map<Int, Int> = terms.groupingBy { it }.eachCount()
-    override fun equals(other: Any?): Boolean {
-        other as Partition
-        return this.counterMap == other.counterMap
-    }
-
-    override fun hashCode(): Int {
-        return counterMap.hashCode()
-    }
-}
+data class Partition(val terms: List<Int>)
